04ff820a8453c43cad6f74fa74c5a26f7b6a68d8
Juan Neri <Juanjo.neri@gmail.com>
2018-06-03 23:28:13 -0700

    3rd attempt

--- Django-Website/mysite/settings.py
+++ Django-Website/mysite/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/home/juanjo_neri/Bazaar/Django-Website/portfolio/static',
-]
+
+import os.path
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
