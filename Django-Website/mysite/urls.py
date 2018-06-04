acd32e4cfc3ce6660db809a29c300bd4b98f4b07
Juan Neri <Juanjo.neri@gmail.com>
2018-06-03 23:40:42 -0700

    lets se

--- Django-Website/mysite/urls.py
+++ Django-Website/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('portfolio.urls')),
     path('admin/', admin.site.urls),
+    staticfiles_urlpatterns(),
 ]
